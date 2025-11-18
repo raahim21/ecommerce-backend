a76d0644ff8fade146a96cd4df5247ad68a05c41
raahim wajid <raahimwajid21@gmail.com>
2025-11-18 14:11:18 +0500

    removed email verification

--- models/User.js
+++ models/User.js
@@ -1,54 +1,88 @@
+// const mongoose = require('mongoose');
+// const bcrypt = require('bcryptjs');
+// const crypto = require('crypto');
+
+// const userSchema = new mongoose.Schema({
+//   name: { type: String, required: true, trim: true },
+//   email: { type: String, required: true, unique: true, lowercase: true },
+//   password: { type: String },
+//   role: {type: String, default:'admin'  },
+//   isVerified: { type: Boolean, default: false },
+//   cart: [
+//     {
+//       productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
+//       variantId: { type: mongoose.Schema.Types.ObjectId, required: true }, // from Product.variants
+//       quantity: { type: Number, required: true, min: 1 },
+//     },
+//   ],
+//   verificationToken: String,
+//   verificationTokenExpires: Date,
+//   passwordChangedAt: Date,
+//   passwordResetToken: String,
+//   passwordResetExpires: Date,
+// }, { timestamps: true });
+
+// userSchema.pre('save', async function (next) {
+//   if (!this.isModified('password') || !this.password) return next();
+
+//   this.password = await bcrypt.hash(this.password, 12);
+
+//   if (!this.isNew) {
+//     this.passwordChangedAt = Date.now() - 1000;
+//   }
+  
+//   next();
+// });
+
+// userSchema.methods.comparePassword = function (pwd) {
+//   return bcrypt.compare(pwd, this.password);
+// };
+
+// userSchema.methods.createPasswordResetToken = function() {
+//   const resetToken = crypto.randomBytes(32).toString('hex');
+
+//   this.passwordResetToken = crypto
+//     .createHash('sha256')
+//     .update(resetToken)
+//     .digest('hex');
+  
+//   this.passwordResetExpires = Date.now() + 10 * 60 * 1000; // 10 minutes
+
+//   return resetToken;
+// };
+
+// module.exports = mongoose.model('User', userSchema);
+
+
+
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
-const crypto = require('crypto');
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true, trim: true },
   email: { type: String, required: true, unique: true, lowercase: true },
-  password: { type: String },
-  role: {type: String, default:'admin'  },
-  isVerified: { type: Boolean, default: false },
+  password: { type: String, required: true },
+  role: { type: String, default: 'admin' },
   cart: [
     {
       productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
-      variantId: { type: mongoose.Schema.Types.ObjectId, required: true }, // from Product.variants
+      variantId: { type: mongoose.Schema.Types.ObjectId, required: true },
       quantity: { type: Number, required: true, min: 1 },
     },
   ],
-  verificationToken: String,
-  verificationTokenExpires: Date,
-  passwordChangedAt: Date,
-  passwordResetToken: String,
-  passwordResetExpires: Date,
 }, { timestamps: true });
 
+// Hash password before saving
 userSchema.pre('save', async function (next) {
-  if (!this.isModified('password') || !this.password) return next();
+  if (!this.isModified('password')) return next();
 
   this.password = await bcrypt.hash(this.password, 12);
-
-  if (!this.isNew) {
-    this.passwordChangedAt = Date.now() - 1000;
-  }
-  
   next();
 });
 
-userSchema.methods.comparePassword = function (pwd) {
-  return bcrypt.compare(pwd, this.password);
-};
-
-userSchema.methods.createPasswordResetToken = function() {
-  const resetToken = crypto.randomBytes(32).toString('hex');
-
-  this.passwordResetToken = crypto
-    .createHash('sha256')
-    .update(resetToken)
-    .digest('hex');
-  
-  this.passwordResetExpires = Date.now() + 10 * 60 * 1000; // 10 minutes
-
-  return resetToken;
+// Compare password method
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password);
 };
 
 module.exports = mongoose.model('User', userSchema);
