aa7248e4ca0c51bb04e65f265d1fc1612cab9501
raahim wajid <raahimwajid21@gmail.com>
2025-11-18 19:31:59 +0500

    final push

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.error(err));
 
-// const PORT = process.env.PORT || 5000;
-const PORT =  5000;
+const PORT = process.env.PORT || 5000;
+// const PORT =  5000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
